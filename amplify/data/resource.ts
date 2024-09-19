5b6131a39a2dabdeecaf62d1e7157eb36322efd7
Jacquat Jonathan <jacquat.jonathan@gmail.com>
2024-09-19 09:50:18 +0200

    Created base schema for the game

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,16 +1,52 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Player: a.model({
+    name: a.string().required(),
+    username: a.string().required(),
+    xp: a.integer().required().default(0),
+    karma: a.integer().required().default(0),
+    levelId: a.id(),
+    level: a.belongsTo('Level', 'levelId'),
+    titleId: a.id(),
+    title: a.belongsTo('Title', 'titleId'),
+  }),
+  Level: a.model({
+    level: a.integer().required(),
+    neededForNextLevel: a.integer().required(),
+    players: a.hasMany('Player', 'levelId'),
+    advantage: a.hasOne('Advantage', 'levelId'),
+  }),
+  Advantage: a.model({
+    description: a.string().required(),
+    levelId: a.id(),
+    level: a.belongsTo('Level', 'levelId'),
+  }),
+  Title: a.model({
+    titleXpId: a.id(),
+    titleXp: a.belongsTo('TitleXp', 'titleXpId'),
+    titleKarmaId: a.id(),
+    titleKarma: a.belongsTo('TitleKarma', 'titleKarmaId'),
+    players: a.hasMany('Player', 'titleId'),
+  }),
+  TitleXp: a.model({
+    name: a.string().required(),
+    needed: a.integer().required(),
+    titles: a.hasMany('Title', 'titleXpId'),
+  }),
+  TitleKarma: a.model({
+    name: a.string().required(),
+    needed: a.integer().required(),
+    titles: a.hasMany('Title', 'titleKarmaId'),
+  }),
+  Quest: a.model({
+    description: a.string().required(),
+    xp: a.integer().required(),
+    karma: a.integer().required(),
+  }),
+  Logs: a
     .model({
-      content: a.string(),
-      isDone: a.boolean().default(false)
+      description: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -20,39 +56,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: 'apiKey',
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { useEffect, useState } from 'react';
+import type { Schema } from '../amplify/data/resource';
+import { generateClient } from 'aws-amplify/data';
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [players, setPlayers] = useState<Array<Schema['Player']['type']>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+    client.models.Player.observeQuery().subscribe({
+      next: (data) => setPlayers([...data.items]),
     });
   }, []);
 
+  /*
   function createTodo() {
     client.models.Todo.create({
-      content: window.prompt("Todo content"),
+      content: window.prompt('Todo content'),
       isDone: false,
     });
   }
@@ -23,26 +24,15 @@ function App() {
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id });
   }
-  console.log(todos);
+*/
+  console.log(players);
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>My players</h1>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <div>
-              <div style={{ paddingRight: 10 }}>{todo.content}</div>
-              <div
-                style={{ paddingRight: 10 }}
-                onClick={() => deleteTodo(todo.id)}
-              >
-                delete
-              </div>
-              <div>{todo.isDone}</div>
-            </div>
-          </li>
+        {players.map((player) => (
+          <li key={player.id}>{player.name}</li>
         ))}
       </ul>
     </main>
