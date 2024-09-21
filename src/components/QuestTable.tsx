79c7273857d8ca49eedf91066cc2bb1cae75b02b
Jacquat Jonathan <jacquat.jonathan@gmail.com>
2024-09-21 16:54:39 +0200

    Schema was false

--- src/components/QuestTable.tsx
+++ src/components/QuestTable.tsx
@@ -1,12 +1,5 @@
 import { useEffect, useState } from 'react';
 import type { Schema } from '../../amplify/data/resource';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
 type QuestTableProps = {
   client: any;
